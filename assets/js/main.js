28880fed3b80f8db09b94de7141c743d68b0b1a1
Dayer5 <61925061+Dayer5@users.noreply.github.com>
2021-09-03 19:31:19 -0600

    Created init function, canvas and linked main.js
    Created init function
    Created 500x500 canvas
    Created variables "canvas" and "ctx"
    Linked main.js to the index.html file

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+//Canvas DOM manipulation
+var canvas = document.getElementById("canvasID")
+var ctx = canvas.getContext("2d")
+
+//Call init function
+init()
+
+//Init Function
+function init(){
+    //Background
+    //Text
+    //Buttons
+
+    //Put the gameSetup() function here
+}
