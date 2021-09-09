95b070ba74ddc5b55b9fb23ec2bfff4f4f5ef644
Dayer5 <61925061+Dayer5@users.noreply.github.com>
2021-09-09 09:34:12 -0600

    Update Init Function, Update index.html, Update main.css
    Added basic menu
    Connected main.css to index.html
    Centered canvas
    Updated title

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,16 @@ init()
 //Init Function
 function init(){
     //Background
+    ctx.fillStyle = "#21b51f"
+    ctx.fillRect(0,0,canvas.width,canvas.height)
     //Text
+    ctx.fillStyle = "black"
+    ctx.font = "40px fantasy"
+    ctx.textAlign = "center"
+    ctx.fillText("ADVENTURE PARK", canvas.width/2,canvas.height/3)
+    ctx.font = "15px Times New Roman"
+    ctx.fillText("Game Made by David J. and Santiago N.", canvas.width/3*2, canvas.height/12*5)
+
     //Buttons
 
     //Put the gameSetup() function here
