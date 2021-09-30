61e60547f70f4b237607ea4aa96f67857186ce93
Dayer5 <61925061+Dayer5@users.noreply.github.com>
2021-09-30 13:59:28 -0600

    Small Fixes

--- assets/js/classes/class.RigidBody.js
+++ assets/js/classes/class.RigidBody.js
@@ -1,12 +1,12 @@
 
 class RigidBody extends Entity{
 
-    constructor(affected_by_gravity){
+    constructor(x, y, width, height, affected_by_gravity){
         super(x, y, width, height)
 
         this.affected_by_gravity = affected_by_gravity
         this.x_speed = 0
-        this.y_speed = 1
+        this.y_speed = 0
         this.gravity = 1
     }   
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ var canvas = document.getElementById("gameCanvas")
 var ctx = canvas.getContext("2d")
 
 //Variables
-var test
 var game_started = false
 var current_location
 var btn_start_game = [canvas.width/2 - 100, canvas.height/2, 200, 50]
@@ -41,7 +40,6 @@ function gameSetup(level){
     //set current location
     current_location = level
     //create entities
-    test = new RigidBody(50, 50, 100, 100, true)
     //start game loop
     update_interval = setInterval(function(){ update() }, 10/1000)
     render_interval = setInterval( function(){ render(ctx) }, 30/1000)
